38a6692f7fa8d5edefa84fb37ab2fee5e97ceba6
PatrickKeijiMonzon <134984495+PatrickKeijiMonzon@users.noreply.github.com>
2025-01-15 09:45:16 +0800

    SampleQuiz

---
+++ untitled/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+
+        System.out.println("Enter your name: ");
+
+        Scanner inputScanner = new Scanner(System.in);
+        String name = inputScanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
+    }
+}
